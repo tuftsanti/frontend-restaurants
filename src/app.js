fcbf2811fe2e7ed1ebd05b4ed0a1ea07da025b21
Andy Riecker <ariecker@gmail.com>
2020-06-08 15:48:26 -0400

    styled and oriented items within fe

--- src/app.js
+++ src/app.js
@@ -2,17 +2,119 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
-import './css/style.css';
+import './scss/style.scss';
 
 const App = (props) => {
-    return (
-        <>
-            <Header />
-            <h1>Hello World</h1>
-            <Footer />
-        </>
-    );
-};
+    // Hook to hold array of restaurants
+    const [restaurants, setRestaurant] = React.useState(null)
+    // Hook to hold edited restaurant
+    const [editThisRestaurant, setEditedRestaurant] = React.useState({
+        name: '',
+        url: '',
+        location: {
+            address: '',
+            locality: '',
+            city: '',
+            zipcode: '00000'
+        }
+    }
+)
 
+// Store jwt
+// const [token, setToken] = React.useState(null)
+
+// Localize storage for jwt
+/* React.useEffect(() => {
+    const checkToken = JSON.parse(window.localStorage.getItem('token'))
+    if (checkToken) {
+        setToken(checkToken)
+    }
+}, [])*/
+
+// GET the list of restaurants
+const getRestaurants = async () => {
+    const response = await fetch('http://localhost:3000/restaurants') /*, {
+        headers: { Authorization: `bearer ${token}` }
+    })*/
+    const result = await response.json()
+    console.log(result)
+    setRestaurant(result)
+}
+
+// Hook to GET from API data
+React.useEffect(() => {
+    // if (token) {
+        getRestaurants()
+    // }
+}, [])
+
+// Select Restaurant
+const selectRestaurant = async (restaurant) => {
+    setEditedRestaurant(restaurant)
+}
+
+// Edit a Restaurant
+const editRestaurant = async (data) => {
+    const response = await fetch(`http://localhost:3000/restaurants/${data._id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': "application/json" /*,
+            Authorization: `bearer ${token}` */
+        },
+        body: JSON.stringify(data)
+    })
+    getRestaurants()
+}
+// Login
+/* const handleLogin = async (data) => {
+    const response = await fetch(`http://localhost:3000/login`, {
+        method: 'POST',
+        headers: {'Content-type': 'application/json'},
+        body: JSON.stringify(data)
+    })
+    const result = await response.json()
+    setToken(result)
+    window.localStorage.setItem('token', JSON.stringify(result))
+}*/
+// Logout
+/* const handleLogout = () => {
+    window.localStorage.removeItem('token')
+    setToken(null)
+    setBookmark(null)
+} */
+
+// Display Page
+return (
+    <>
+        <Header /* <button onClick={handleLogout}>Logout</button> */ />
+        <div className="App">
+            App
+            <div className="App__sidebar">
+                <h3>Add Filters Here</h3>
+            </div>
+            <div className="App__mainview">
+                <h2>Local Restaurants</h2>
+                <ul className="App__mainview--grid">
+                {restaurants ? restaurants.map((restaurant) => {
+                    // restaurant.url = `http://`+restaurant.url
+                    return (
+                        <li key={restaurant._id} className="__individualRestaurant">
+                            <a href="" className="--pic">{restaurant.picture}</a>
+                            <h3 className="--name">{restaurant.name}</h3>
+                            <button onClick={() => {
+                                selectRestaurant(restaurant)
+                            }}>Edit this restaurant</button>
+                        </li>
+                    )
+                }) : `Searching Your Restaurants`
+                }
+                </ul>
+            </div>
+        </div>
+    </>
+)
+
+
+}   
 const target = document.getElementById('app');
 ReactDOM.render(<App />, target);
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 export default (props) => {
     return (
         <>
-            <h1>Header</h1>
+            <h1>Restaurants</h1>
         </>
     );
 };
