59b81f57e767e7b8a6eab209d20a05a566df6cfd
Andy Riecker <ariecker@gmail.com>
2020-06-11 14:24:23 -0400

    local dev in case

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -29679,7 +29679,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56289" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39889" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
