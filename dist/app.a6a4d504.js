04f8e2ce755ea6c3becbf35325cccc9740f563cd
Andy Riecker <ariecker@gmail.com>
2020-06-12 17:53:07 -0400

    updated fe

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -46629,7 +46629,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34777" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46253" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
