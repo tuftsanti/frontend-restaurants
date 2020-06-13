5989dd5e48deb46aeb0f93fa62b371ed0495d624
Sam Bevan <samueleganbevan@gmail.com>
2020-06-13 17:54:16 -0400

    update from dev

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -46814,7 +46814,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65374" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51632" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
