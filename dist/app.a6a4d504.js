0df3b52f08becec6f63916dbc0fab77f9a849c4b
Sam Bevan <samueleganbevan@gmail.com>
2020-06-15 00:29:18 -0400

    changes from origin master

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -46925,7 +46925,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61101" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61128" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
